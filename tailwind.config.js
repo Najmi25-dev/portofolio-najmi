f2c7db26173da0c3e6456b8673e4d8482ad19c70
Muhammad Najmi Muhtaram <126326205+Najmi25-dev@users.noreply.github.com>
2026-01-07 11:15:34 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./**/*.js"],
+  content: [
+  './index.html',
+  './**/*.{html,js}'
+],
+
 
   safelist: [
     "flex",
